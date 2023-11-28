952c81697ed8ad74057d99aa2c0e254ff05bd6b1
Diaa-E <blarb21@yahoo.com>
2023-11-28 11:26:04 +0200

    Create import barrel for link icons

---
+++ src/iconsBarrel.js
@@ -0,0 +1,9 @@
+import gitHubIcon from "./assets/link_icons/github.svg";
+import behanceIcon from "./assets/link_icons/behance.svg";
+
+const linkIcons = {
+    gitHub: gitHubIcon,
+    behance: behanceIcon,
+};
+
+export default linkIcons;
