3ae451a5df55aa139f935ae3391468318f56e661
Diaa-E <blarb21@yahoo.com>
2023-12-06 01:43:34 +0200

    Add more link options

--- src/linkIconsBarrel.js
+++ src/linkIconsBarrel.js
@@ -1,16 +1,41 @@
 import gitHubIcon from "./assets/link_icons/github.svg";
 import behanceIcon from "./assets/link_icons/behance.svg";
+import facebookIcon from "./assets/link_icons/facebook.svg";
+import youtubeIcon from "./assets/link_icons/youtube.svg";
+import linkedinIcon from "./assets/link_icons/linkedin.svg";
+import instagramIcon from "./assets/link_icons/instagram.svg";
+import artstationLogo from "./assets/link_icons/artstation.svg";
 import otherIcon from "./assets/link_icons/other.svg";
 
 const linkIcons = [
     {
-        name: "github",
+        name: "Github",
         icon: gitHubIcon,
     },
     {
-        name: "behance",
+        name: "Behance",
         icon: behanceIcon,
     },
+    {
+        name: "Facebook",
+        icon: facebookIcon,
+    },
+    {
+        name: "YouTube",
+        icon: youtubeIcon
+    },
+    {
+        name: "LinkedIn",
+        icon: linkedinIcon
+    },
+    {
+        name: "Instagram",
+        icon: instagramIcon
+    },
+    {
+        name: "Art Station",
+        icon: artstationLogo
+    }
 ].sort((a, b) => {
     if (a.name < b.name)
     {
