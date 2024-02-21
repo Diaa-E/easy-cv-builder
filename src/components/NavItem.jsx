208c7eb48f8891230f1da46322b9a8bea027dea3
Diaa-E <blarb21@yahoo.com>
2024-02-21 14:14:52 +0200

    Fix tab id being used in alt text

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,11 +1,11 @@
 import "../styles/NavItem.css";
 
-export default function NavItem({iconUrl, id, selected = false, onClick = () => {}})
+export default function NavItem({iconUrl, title, id, selected = false, onClick = () => {}})
 {
     return (
         <>
             <button className={`nav-item ${selected? "selected" : ""}`} onClick={() => onClick(id)}>
-                <img src={iconUrl} alt={`Icon for ${id} tab`} />
+                <img src={iconUrl} alt={`${title} tab icon`} />
             </button>
         </>
     )
