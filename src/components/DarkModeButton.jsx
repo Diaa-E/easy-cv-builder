e60bf731685d0ca3c20ca7e4f53a0cf8dc7eae2b
Diaa-E <blarb21@yahoo.com>
2024-02-24 17:56:50 +0200

    Animate dark mode button on switch

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -6,8 +6,13 @@ export default function DarkModeButton({darkMode, onClick})
     return (
         <button onClick={onClick} className={`dark-mode-button`}>
             <img
-                className={`dark-mode-icon ${darkMode ? "yellow" : "blue"}`}
-                src={darkMode ? appIcons.light : appIcons.dark}
+                className={`dark-mode-icon yellow ${darkMode ? "active" : "inactive"}`}
+                src={appIcons.light}
+                alt="dark mode button"
+            />
+            <img
+                className={`dark-mode-icon blue ${darkMode ? "inactive" : "active"}`}
+                src={appIcons.dark}
                 alt="dark mode button"
             />
         </button>
