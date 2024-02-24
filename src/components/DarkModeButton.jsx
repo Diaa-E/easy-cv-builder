40ec9b590761d2add97bdf79fe25c8c3409d1aa4
Diaa-E <blarb21@yahoo.com>
2024-02-24 17:26:51 +0200

    Add styles to dark mode button

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -4,7 +4,7 @@ import appIcons from "../appIconsBarrel";
 export default function DarkModeButton({darkMode, onClick})
 {
     return (
-        <button onClick={onClick} className={`dark-mode-button ${darkMode ? "yellow" : "blue"}`}>
+        <button onClick={onClick} className={`dark-mode-button`}>
             <img
                 className={`dark-mode-icon ${darkMode ? "yellow" : "blue"}`}
                 src={darkMode ? appIcons.light : appIcons.dark}
