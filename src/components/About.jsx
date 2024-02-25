d68aa903c326d3f8e0c48d098f5b789e21df1c26
Diaa-E <blarb21@yahoo.com>
2024-02-25 13:09:27 +0200

    Fix empty list text color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About({version, enabled = true})
     {
         return (
             <div role="region" className="items-container">
-                <h2>Easy CV Builder v{version}</h2>
+                <h2 className="app-title">Easy CV Builder v{version}</h2>
                 <p>Created by Diaa E.</p>
                 <p>Project's Repo <a href="https://github.com/Diaa-E/easy-cv-builder">Easy CV Builder repo</a></p>
                 <p>My Github <a href="https://github.com/Diaa-E">Diaa E. on github</a></p>
