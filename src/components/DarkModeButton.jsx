d8b5555e8174aa36cc0b945835096d03c1115e76
Diaa-E <blarb21@yahoo.com>
2024-02-25 13:24:35 +0200

    Make dark mode button's icon represent current active theme

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -6,12 +6,12 @@ export default function DarkModeButton({darkMode, onClick})
     return (
         <button onClick={onClick} className={`dark-mode-button`}>
             <img
-                className={`dark-mode-icon yellow ${darkMode ? "active" : "inactive"}`}
+                className={`dark-mode-icon yellow ${darkMode ? "inactive" : "active"}`}
                 src={appIcons.light}
                 alt="dark mode button"
             />
             <img
-                className={`dark-mode-icon blue ${darkMode ? "inactive" : "active"}`}
+                className={`dark-mode-icon blue ${darkMode ? "active" : "inactive"}`}
                 src={appIcons.dark}
                 alt="dark mode button"
             />
