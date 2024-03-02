542c420467b9dd24a8a5e9f843ae8d425ca48b4e
Diaa-E <blarb21@yahoo.com>
2024-03-02 19:27:53 +0200

    Return focus to text box when clear field is clicked

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,14 +1,18 @@
 import "../styles/TextInput.css";
 import appIcons from "../appIconsBarrel";
+import { useRef } from "react";
 
 export default function TextInput({labelText, text, placeholder, id, onChange = () => {}, clearField})
 {
+    const ref = useRef(null);
+
     return (
         <div data-testid="text-input" className="text-input-container">
             <label className="text-input-label" htmlFor={id}>
                 {labelText}
             </label>
             <input
+                ref={ref}
                 id={id}
                 type="text"
                 value={text}
@@ -19,7 +23,11 @@ export default function TextInput({labelText, text, placeholder, id, onChange = 
             />
             {
                 !(text === "") &&
-                <button data-testid="clear-field-button" title="Clear field" onClick={clearField} className="clear-button">
+                <button data-testid="clear-field-button" title="Clear field" onClick={() => {
+                    ref.current.focus();
+                    clearField();
+                }}
+                className="clear-button">
                     <img src={appIcons.clear} alt="Clear field button icon" />
                 </button>
             }
