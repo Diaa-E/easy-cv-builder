234e2b7769dfd7d4fa29c2305184b12ebeb894ad
Diaa-E <blarb21@yahoo.com>
2024-03-02 23:52:57 +0200

    Remove use context call

--- src/components/ListItemText.jsx
+++ src/components/ListItemText.jsx
@@ -3,12 +3,9 @@ import appIcons from "../appIconsBarrel";
 
 import ItemButton from "./ItemButton";
 import ItemControls from "./ItemControls";
-import { useContext } from "react";
 
 export default function ListItemText({firstLine, secondLine, hidden = false, id, toggleHide, toggleEdit, deleteItem, moveItemUp})
 {
-    const dialogSetter = useContext(DialogContext);
-
     return (
         <div className={hidden ? "list-item-hidden" : "list-item"}>
             <ItemButton onClick={() => deleteItem(id)} text="delete item" imgPath={appIcons.delete} colorClass="item-button-danger"/>
