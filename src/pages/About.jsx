9ff373ccea54b58cfdb3c23cac78c893b7e247ba
Diaa-E <blarb21@yahoo.com>
2024-03-07 19:55:19 +0200

    Open external links in a new tab

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,9 +6,9 @@ export default function About({version, enabled = true})
             <div role="region" className="items-container">
                 <h2 className="app-title">Easy CV Builder v{version}</h2>
                 <p>©️ {(new Date).getFullYear()} Diaa E.</p>
-                <p>Changes <a href="https://github.com/Diaa-E/easy-cv-builder/blob/main/CHANGELOG.md">Change log</a></p>
-                <p>Project's Repo <a href="https://github.com/Diaa-E/easy-cv-builder">Easy CV Builder repo</a></p>
-                <p>My Github <a href="https://github.com/Diaa-E">Diaa E. on github</a></p>
+                <p>Changes <a target="_blank" href="https://github.com/Diaa-E/easy-cv-builder/blob/main/CHANGELOG.md">Change log</a></p>
+                <p>Source code <a target="_blank" href="https://github.com/Diaa-E/easy-cv-builder">Easy CV Builder repository</a></p>
+                <p>My Github <a target="_blank" href="https://github.com/Diaa-E">Diaa E. on github</a></p>
             </div>
         )
     }
