051dbbb26a889a97ae17acedbe1d58ac8bf7bc70
Diaa-E <blarb21@yahoo.com>
2024-04-16 07:35:13 +0200

    Remove dummy component and test

--- src/components/Dummy.jsx
+++
@@ -1,8 +0,0 @@
-export default function Dummy({handleClick})
-{
-    return (
-        <div data-testid="dummy">
-            <button onClick={(handleClick)}>Dummy Button</button>
-        </div>
-    )
-}
