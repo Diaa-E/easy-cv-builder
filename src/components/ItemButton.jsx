5181c5e1e329395fad827e65813a53af2dac93b9
Diaa-E <blarb21@yahoo.com>
2024-04-16 09:36:56 +0200

    Improve item button accessibility

--- src/components/ItemButton.jsx
+++ src/components/ItemButton.jsx
@@ -3,8 +3,8 @@ import "../styles/ItemButton.css";
 export default function ItemButton({colorClass = "", imgPath, text, onClick = () => {}, toolTip = `${text}`})
 {
     return (
-        <button title={toolTip} onClick={onClick} className={`item-button ${colorClass}`}>
-            <img src={imgPath} alt={`${text} button icon`} />
+        <button aria-label={text} title={toolTip} onClick={onClick} className={`item-button ${colorClass}`}>
+            <img aria-hidden src={imgPath} alt={`${toolTip} button icon`} />
         </button>
     )
 }
