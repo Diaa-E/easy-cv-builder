e31e3d24929d91ba76f7382cd9b95a9d5c7e2dfc
Diaa-E <blarb21@yahoo.com>
2024-04-16 07:33:39 +0200

    Improve dark mode button accessibility

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -4,13 +4,20 @@ import appIcons from "../data/appIconsBarrel";
 export default function DarkModeButton({darkMode, onClick})
 {
     return (
-        <button title="Dark mode" onClick={onClick} className={`dark-mode-button`}>
+        <button
+            aria-label={`use ${darkMode ? "light" : "dark"} theme`}
+            title={`Use ${darkMode ? "light" : "dark"} theme`}
+            onClick={onClick}
+            className={`dark-mode-button`}
+        >
             <img
+                aria-hidden
                 className={`dark-mode-icon yellow ${darkMode ? "inactive" : "active"}`}
                 src={appIcons.light}
                 alt="dark mode button"
             />
             <img
+                aria-hidden
                 className={`dark-mode-icon blue ${darkMode ? "active" : "inactive"}`}
                 src={appIcons.dark}
                 alt="dark mode button"
