65da4b0967202ce335eb9fce913331a60cfdf546
Diaa-E <blarb21@yahoo.com>
2024-04-17 09:15:41 +0200

    Remove duplicate attribute

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -3,7 +3,12 @@ import "../styles/ProgressBar.css";
 export default function ProgressBar({level, disabled})
 {
     return (
-        <div role="progressbar" aria-label="proficiency level" aria-valuenow={`${level}%`} className={`progress-bar-outer ${disabled ? "disabled" : ""}`} role="progressbar">
+        <div
+            role="progressbar"
+            aria-label="proficiency level"
+            aria-valuenow={`${level}%`}
+            className={`progress-bar-outer ${disabled ? "disabled" : ""}`}
+        >
             <span className="progress-bar-inner" style={{width: `${level}%`}}></span>
         </div>
     )
