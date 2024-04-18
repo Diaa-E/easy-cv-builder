747d6e60280705c40dc8f321c15230cd16f4720a
Diaa-E <blarb21@yahoo.com>
2024-04-18 11:47:53 +0200

    Remove useless tests - Update tests to component changes

--- src/components/__tests__/SelectInput.test.jsx
+++ src/components/__tests__/SelectInput.test.jsx
@@ -1,34 +1,12 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import SelectInput from "../SelectInput";
 
 describe("SelectInput component", () => {
 
-    it("Renders in the DOM", () => {
-
-        render(<SelectInput options={[]}/>);
-
-        expect(screen.getByTestId("select-input")).toBeInTheDocument();
-    });
-
-    it("Uses id prop for select element id", () => {
-
-        render(<SelectInput id={"select"} options={[]}/>);
-
-        expect(screen.getByRole("listbox").id).toBe("select")
-    });
-
-    it("Renders a label with the text from props", () => {
-
-        render(<SelectInput options={[]} labelText={"select"}/>);
-
-        expect(screen.getByText("select")).toBeInTheDocument();
-        expect(screen.getByText("select").nodeName).toBe("LABEL");
-    });
-
-    it("Renders options with value and name pairs", () => {
-
-        const mockOptions = [
+    function createOptions()
+    {
+        return [
             {
                 name: "option1",
                 value: "1",
@@ -38,80 +16,51 @@ describe("SelectInput component", () => {
                 value: "2"
             }
         ];
+    }
 
-        render(<SelectInput options={mockOptions} optionNameKey={"name"} optionValueKey={"value"}/>);
+    it("Uses id prop for select element id", () => {
 
-        expect(screen.getAllByRole("option").length).toBe(2);
-        screen.getAllByRole("option").forEach((option, index) => {
+        render(<SelectInput id={"select"} options={[]}/>);
 
-            expect(option.textContent).toBe(mockOptions[index].name);
-            expect(option.value).toBe(mockOptions[index].value);
+        expect(screen.queryByRole("select").id).toBe("select")
+    });
+
+    it("Labels select input with text from props", () => {
+
+        render(<SelectInput options={[]} labelText={"select"}/>);
+
+        expect(screen.queryByText("select")).toBeInTheDocument();
+    });
+
+    it("Renders options with value and name pairs", () => {
+
+        const options = createOptions();
+        render(<SelectInput options={options}/>);
+
+        expect(screen.queryAllByRole("option").length).toBe(2);
+        screen.queryAllByRole("option").forEach((option, index) => {
+
+            expect(option.textContent).toBe(options[index].name);
+            expect(option.value).toBe(options[index].value);
         })
     });
 
     it("Selects option from prop", () => {
 
-        const mockOptions = [
-            {
-                name: "option1",
-                value: "1",
-            },
-            {
-                name: "option2",
-                value: "2",
-            }
-        ];
+        const options = createOptions();
+        render(<SelectInput selected={"2"} options={options}/>);
 
-        render(<SelectInput selected={"2"} options={mockOptions} optionNameKey={"name"} optionValueKey={"value"}/>);
-
-        expect(screen.getByRole("listbox").value).toBe("2");
+        expect(screen.queryByRole("select", {value: "2"})).toBeInTheDocument();
     });
 
     it("Calls onChange function when selected option is changed", () => {
 
-        const mockOptions = [
-            {
-                name: "option1",
-                value: "1",
-            },
-            {
-                name: "option2",
-                value: "2",
-            },
-            {
-                name: "option3",
-                value: "3",
-            }
-        ];
-
+        const options = createOptions();
         const onChange = vi.fn();
-        render(<SelectInput onChange={onChange} selected={"2"} options={mockOptions} optionNameKey={"name"} optionValueKey={"value"}/>);
-        const select = screen.getByRole("listbox");
-        fireEvent.change(select, {target: {value: "3"}});
+        render(<SelectInput onChange={onChange} selected={"2"} options={options}/>);
+        const select = screen.queryByRole("select");
+        fireEvent.change(select, {target: {value: "1"}});
 
         expect(onChange).toHaveBeenCalledOnce();
-    });
-
-    it("Does not call onChange function when selected option does not change", () => {
-
-        const mockOptions = [
-            {
-                name: "option1",
-                value: "1",
-            },
-            {
-                name: "option2",
-                value: "2",
-            },
-            {
-                name: "option3",
-                value: "3",
-            }
-        ];
-
-        const onChange = vi.fn();
-        render(<SelectInput onChange={onChange} selected={"2"} options={mockOptions} optionNameKey={"name"} optionValueKey={"value"}/>);
-
-        expect(onChange).not.toHaveBeenCalled();
     });
 });
