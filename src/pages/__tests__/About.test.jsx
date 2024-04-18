95b8d2f405b775b672eb5ae5721a85914cb9c2ab
Diaa-E <blarb21@yahoo.com>
2024-04-18 13:56:38 +0200

    Remove useless tests - update tests to component changes

--- src/pages/__tests__/About.test.jsx
+++ src/pages/__tests__/About.test.jsx
@@ -1,27 +1,33 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import About from "../About";
 
 describe("About component", () => {
 
-    it("Renders in DOM", () => {
+    vi.mock("../../data/meta.js", () => {
+
+        return {
+            meta: {
+                title: "easy cv builder",
+                version: "1.0.0",
+                sourceUrl: "source"
+            }
+        }
+    });
+
+    it("Renders heading with app title and version", () => {
 
         render(<About/>);
 
-        expect(screen.getByRole("region")).toBeInTheDocument();
+        expect(screen.queryByRole("heading", {name: /easy\scv\sbuilder/i})).toBeInTheDocument();
+        expect(screen.queryByRole("heading", {name: /v1\.0\.0/i})).toBeInTheDocument();
     });
 
-    it("Renders a title with the app's name followed by v (for version)", () => {
+    it("Renders a link to app source code", () => {
 
-        render(<About/>);
+        render(<About />);
 
-        expect(screen.getByRole("heading").textContent).toBe("Easy CV Builder v");
-    });
-
-    it("Shows version number from props", () => {
-
-        render(<About version={"1.0.0"} />);
-
-        expect(screen.getByRole("heading").textContent).toBe("Easy CV Builder v1.0.0");
+        expect(screen.queryByRole("link", {name: /source/i})).toBeInTheDocument();
+        expect(screen.queryByRole("link", {name: /source/i}).href).toContain("source");
     });
 });
