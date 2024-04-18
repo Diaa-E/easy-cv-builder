4348b122d8e015fd02c19de6f586faa5b22dcfbf
Diaa-E <blarb21@yahoo.com>
2024-04-18 13:27:46 +0200

    IMprove toggle all button accessibility

--- src/components/ToggleAllButton.jsx
+++ src/components/ToggleAllButton.jsx
@@ -3,8 +3,13 @@ import "../styles/ToggleAllButton.css";
 export default function ToggleAllButton({icon, onClick, toolTip, colorClasses = []})
 {
     return (
-        <button onClick={onClick} className={`toggle-all-button ${colorClasses.join(" ")}`} title={toolTip}>
-            <img src={icon} alt={toolTip + " button icon"} />
+        <button
+            onClick={onClick}
+            className={`toggle-all-button ${colorClasses.join(" ")}`}
+            title={toolTip}
+            aria-label={toolTip}
+        >
+            <img aria-hidden src={icon} alt={toolTip + " button icon"} />
         </button>
     )
 }
