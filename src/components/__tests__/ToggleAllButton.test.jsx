76a5b521491a12915a0869b97b4bcab973104517
Diaa-E <blarb21@yahoo.com>
2024-04-18 13:28:06 +0200

    Remove useless tests - update tests to component changes

--- src/components/__tests__/ToggleAllButton.test.jsx
+++ src/components/__tests__/ToggleAllButton.test.jsx
@@ -1,66 +1,47 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import ToggleAllButton from "../ToggleAllButton";
-import userEvent from "@testing-library/user-event";
 
 describe("Toggle all button component", () => {
-
-    it("Renders in the DOM", () => {
-
-        render(<ToggleAllButton/>);
-
-        expect(screen.getByRole("button")).toBeInTheDocument();
-    });
 
     it("Renders an image with prop icon path", () => {
 
         render(<ToggleAllButton icon={"path"}/>);
 
-        expect(screen.getByRole("button").childNodes[0].nodeName).toBe("IMG");
-        expect(screen.getByRole("button").childNodes[0].src).toContain("path");
+        expect(screen.getByRole("img", {hidden: true}).src).toContain("path");
     });
 
     it("Adds classes from props to the button", () => {
 
-        render(<ToggleAllButton colorClasses={["color1", "color2"]}/>);
+        render(<ToggleAllButton toolTip={"click"} colorClasses={["color1", "color2"]}/>);
 
-        expect(screen.getByRole("button")).toHaveClass("color1", "color2");
+        expect(screen.getByRole("button", {name: /click/i})).toHaveClass("color1", "color2");
     });
 
     it("Adds tooltip prop to the title of the button", () => {
 
-        render(<ToggleAllButton toolTip={"click here"}/>);
+        render(<ToggleAllButton toolTip={"click"}/>);
 
-        expect(screen.getByRole("button").title).toBe("click here");
+        expect(screen.getByRole("button", {name: /click/i}).title).toBe("click");
     }); 
 
-    it("Calls onClick function once when clicked", async () => {
+    it("Calls onClick function once when clicked", () => {
 
-        const user = userEvent.setup();
         const onClick = vi.fn();
-        render(<ToggleAllButton onClick={onClick}/>);
-        const button = screen.getByRole("button");
-        await user.click(button);
+        render(<ToggleAllButton toolTip={"click"} onClick={onClick}/>);
+        const button = screen.getByRole("button", {name: /click/i});
+        fireEvent.click(button);
 
         expect(onClick).toHaveBeenCalledOnce();
     }); 
 
-    it("Calls onClick function once when the icon is clicked", async () => {
+    it("Calls onClick function once when the icon is clicked", () => {
 
-        const user = userEvent.setup();
         const onClick = vi.fn();
-        render(<ToggleAllButton onClick={onClick}/>);
-        const icon = screen.getByRole("img");
-        await user.click(icon);
+        render(<ToggleAllButton toolTip={"click"} onClick={onClick}/>);
+        const icon = screen.getByRole("img", {hidden: true});
+        fireEvent.click(icon);
 
         expect(onClick).toHaveBeenCalledOnce();
     });
-
-    it("Does not call onClick function when not clicked", () => {
-
-        const onClick = vi.fn();
-        render(<ToggleAllButton onClick={onClick}/>);
-
-        expect(onClick).not.toHaveBeenCalled();
-    })
 });
