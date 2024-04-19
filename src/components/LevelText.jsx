d900c82d5b008599e87b1c4ecb2f667f13fbbf03
Diaa-E <blarb21@yahoo.com>
2024-04-19 08:31:06 +0200

    Use CSS modules for level text component

--- src/components/LevelText.jsx
+++ src/components/LevelText.jsx
@@ -1,4 +1,4 @@
-import "../styles/LevelText.css";
+import styles from "../styles/LevelText.module.css";
 import { calculateTextLevel } from "../utils/calculateTextLevel";
 
 export default function LevelText({itemData, textLevels = [{name: "High", min: 100}, {name: "Medium", min: 50}, {name: "Low", min: 0}]})
@@ -6,7 +6,7 @@ export default function LevelText({itemData, textLevels = [{name: "High", min: 1
     /* textLevels must be passed in decending order for the matching to work */
     return (
         <>
-            <p aria-label="proficiency level" className="level-text">
+            <p aria-label="proficiency level" className={styles["level-text"]}>
             {
                 calculateTextLevel(textLevels, itemData.level)
             }
