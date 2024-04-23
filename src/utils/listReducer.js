2615c942a3d2045969840a1be4b3219739a8d4cb
Diaa-E <blarb21@yahoo.com>
2024-04-23 14:49:38 +0200

    Add a list reducer function

---
+++ src/utils/listReducer.js
@@ -0,0 +1,30 @@
+import { deleteItem, moveItemUp, toggleHide, updateItems } from "./arrayFunctions";
+import { isEmptySection } from "./emptySectionDetector";
+import { toggleHideSection } from "./toggleHideSection";
+
+export default function reduceList(list, action)
+{
+    switch (action.type)
+    {
+        case "toggleHideItem": return [
+            ...toggleHide(action.itemId, list)
+        ];
+        case "toggleHideAll": return [
+            ...toggleHideSection(list, !isEmptySection(list))
+        ];
+        case "moveUp": return [
+            ...moveItemUp(action.itemId, list)
+        ];
+        case "deleteItem": return [
+            ...deleteItem(action.itemId, list)
+        ];
+        case "deleteAll": return [];
+        case "updateList": return [
+            ...updateItems(action.newItem, list)
+        ];
+        case "reset": return [
+            ...action.defaultList
+        ];
+        default: throw new Error ("Invalid action: " + action.type);
+    }
+}
