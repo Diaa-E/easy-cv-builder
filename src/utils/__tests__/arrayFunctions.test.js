d63441e8fae27ce12ddd2c89af5a20a6f0a364b3
Diaa-E <blarb21@yahoo.com>
2024-04-24 14:02:16 +0200

    Move toggle hide secton function to array functions module

--- src/utils/__tests__/arrayFunctions.test.js
+++ src/utils/__tests__/arrayFunctions.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { deleteItem, moveItemUp, toggleHide, updateItems } from "../arrayFunctions";
+import { deleteItem, moveItemUp, toggleHide, updateItems, toggleHideSection } from "../arrayFunctions";
 
 describe("Toggle hide function", () => {
 
@@ -106,5 +106,79 @@ describe("Move item up function", () => {
     it("Does nothing when passed ID matches first item in array", () => {
 
         expect(moveItemUp("1", setup())).toEqual(setup());
+    });
+});
+
+describe("Toggle hide all items function" , () => {
+
+    function setup()
+    {
+        return [
+            {
+                value: "item1",
+                hidden: false,
+            },
+            {
+                value: "item2",
+                hidden: true,
+            },
+            {
+                value: "item3",
+                hidden: true,
+            },
+            {
+                value: "item4",
+                hidden: false,
+            },
+        ]
+    }
+
+    it("Hides all items when passed true", () => {
+
+        expect(toggleHideSection(setup(), true)).toEqual([
+            {
+                value: "item1",
+                hidden: true,
+            },
+            {
+                value: "item2",
+                hidden: true,
+            },
+            {
+                value: "item3",
+                hidden: true,
+            },
+            {
+                value: "item4",
+                hidden: true,
+            },
+        ]);
+    });
+
+    it("Shows all items when passed false", () => {
+
+        expect(toggleHideSection(setup(), false)).toEqual([
+            {
+                value: "item1",
+                hidden: false,
+            },
+            {
+                value: "item2",
+                hidden: false,
+            },
+            {
+                value: "item3",
+                hidden: false,
+            },
+            {
+                value: "item4",
+                hidden: false,
+            },
+        ]);
+    });
+
+    it("Returns an empty array when passed an empty array regradless of hide flag", () => {
+
+        expect(toggleHideSection([])).toEqual([]);
     });
 });
--- src/utils/arrayFunctions.js
+++ src/utils/arrayFunctions.js
@@ -55,3 +55,15 @@ export function moveItemUp(targetId, dataArray)
 
   return newArray;
 }
+
+export function toggleHideSection(itemsArray, hide)
+{
+    const newArray = JSON.parse(JSON.stringify(itemsArray));
+
+    newArray.forEach(item => {
+
+        hide ? item.hidden = true : item.hidden = false;
+    });
+
+    return newArray;
+}
