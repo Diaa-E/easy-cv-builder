b439ade9db9558e859b450de6383baf04d07a1f3
Diaa-E <blarb21@yahoo.com>
2024-06-05 17:32:15 +0300

    Write unit tests for deep copy utility function

--- src/utils/__tests__/utility.test.js
+++ src/utils/__tests__/utility.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { isBright } from "../utility";
+import { deepCopy, isBright } from "../utility";
 
 describe("Brightness sensor", () => {
 
@@ -11,5 +11,47 @@ describe("Brightness sensor", () => {
     it("Returns false for black", () => {
 
         expect(isBright("#000000")).toBe(false);
+    });
+});
+
+describe("Deep copy", () => {
+
+    it("Returns a new object with the same key-value pairs", () => {
+
+        const sampleObject = {
+            key1: "value1",
+            key2: "value2"
+        };
+
+        expect(deepCopy(sampleObject)).not.toBe(sampleObject);
+        expect(deepCopy(sampleObject)).toEqual(sampleObject);
+    });
+
+    it("Copies deeply nested objects", () => {
+
+        const sampleObject = {
+            level1: {
+                level2: {
+                    level3: {
+                        key1: "value1",
+                        key2: "value2"
+                    }
+                }
+            }
+        };
+
+        expect(deepCopy(sampleObject)).not.toBe(sampleObject);
+        expect(deepCopy(sampleObject)).toEqual(sampleObject);
+    });
+
+    it("Returns a new array with the same items", () => {
+
+        const sampleArray = [
+            "item1",
+            "item2"
+        ];
+
+        expect(deepCopy(sampleArray)).not.toBe(sampleArray);
+        expect(deepCopy(sampleArray)).toEqual(sampleArray);
     });
 });
