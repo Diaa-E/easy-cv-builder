3bf3887ac871d01f6a8d94101a9a12263e2c6e05
Diaa-E <blarb21@yahoo.com>
2024-06-13 06:34:09 +0300

    Remove unused import

--- src/pages/__tests__/Education.test.jsx
+++ src/pages/__tests__/Education.test.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, queryAllByRole, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import Education from "../Education";
 import sampleInfo from "../../data/sampleInfo";
