cf87399eba5ab04e1021aa15052346eae0e01bb2
Diaa-E <blarb21@yahoo.com>
2024-09-05 09:32:35 +0300

    Improve classnames readibility

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -7,7 +7,7 @@ export default function NavItem({iconUrl, title, selected = false, onClick = () 
             <button
                 aria-label={`go to ${title}`}
                 title={title}
-                className={[styles["nav-item"], selected? styles["selected"] : ""].join(" ")}
+                className={`${styles["nav-item"]} ${selected? styles["selected"] : ""}`}
                 onClick={onClick}
             >
                 <img aria-hidden src={iconUrl} alt={`${title} tab icon`} />
